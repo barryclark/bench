58a7778f674576c5e63046c67d19bebdd1857daa
msacca <msacca@tinyfactory.co>
2014-08-19 10:37:14 -0700

    v2 updates

--- js/app.js
+++ js/app.js
@@ -45,6 +45,7 @@ var cachingData = new function () {
 
 var showing = 0,
     user = $('#lookup-player'),
+    themeInput = $('#toggle'),
     userName = cachingData.getUsername(),
     shotcount = 12,
     page = 1,
@@ -68,6 +69,7 @@ var drib = {
             var url = drib.host + drib.popular + "?per_page=" + shotcount + "&page=" + page;
             console.log('####' + url);
             var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from json where url="' + url + '"') + '&format=json&callback=cbfunc';
+            console.log(yql);
         } else if (from === 'debuts') {
             var url = drib.host + drib.debuts + "?per_page=" + shotcount + "&page=" + page;
             var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from json where url="' + url + '"') + '&format=json&callback=cbfunc';
@@ -81,13 +83,17 @@ var drib = {
             if (how === 'new') {
                 $('.subInfo').show().html("<span style='color:#68a4f5'>searching...<span>");
             }
-            var url = drib.host + drib.player + playerId + '/shots/following' + "?per_page=" + shotcount + "&page=" + page;
+            var url = drib.host + drib.player + playerId + '/shots/following' + '?per_page=' + shotcount + '&page=' + page;
             console.log('####' + url);
-            var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from json where url="' + url + '"') + '&format=json&callback=cbfunc';
+            var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from json where url="'+url+'"')+'&format=json&callback=cbfunc';
         }
         this.req(yql, function (data) {
 //            console.log('how:'+ how);
+            if (page === 1) {
+                getShots.getAd();
+            }
             getShots.buildShots(data.query.results.json.shots, how);
+
         });
     },
 
@@ -177,12 +183,55 @@ var drib = {
                 $('.following').addClass('active').siblings().removeClass('active');
             }
         });
+    },
+    activeTheme:function() {
+        var theme = localStorage.getItem('theme');
+        if(!theme){
+            $('body').removeClass('dark').addClass('white');
+            themeInput.prop('checked', false);
+        }
+        else if(theme === 'dark') {
+            $('body').removeClass('white').addClass('dark');
+            themeInput.prop('checked', true);
+        } else {
+            $('body').removeClass('dark').addClass('white');
+            themeInput.prop('checked', false);
+        }
     }
 };
-
-
+var singleShot = '';
 var getShots = {
     container:$('#picsList'),
+    getAd: function() {
+        //insert add into a random position 2 per 11
+        
+        var ad = 'CK7IP';
+        $.ajax({
+            type:'get',
+            url:'https://srv.buysellads.com/ads/' + ad + '.json',
+            success:Success,
+            error:Error,
+            dataType:'jsonp'
+        });
+        function Success(res) {
+            if(res.ads[0].statlink) {
+                clickUrl = res.ads[0].statlink;
+            } else {
+                clickUrl = res.ads[0].statlink_default;
+            } 
+            var adId = res.ads[0].shotId,
+                //clickUrl = if(res.ads[0].statlink) {res.ads[0].statlink} else {res.ads[0].statlink_default},
+                clickId = clickUrl.substring(clickUrl.lastIndexOf('/') + 1),
+                shotUrl = drib.host + drib.shot + adId,
+                yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from json where url="' + shotUrl + '"') + '&format=json&callback=cb',
+                html = res.ads[0].html;
+
+                singleShot += '' + res.ads[0].html + '<div id="ad" class="animate fadeInUp item"><div id="fusion_ad" data-clickid="' + clickId + '">' +
+                '       <a class="poweredBy" href="http://fusionads.net">Powered by Fusion</a>' +
+                '   </div>';
+                '</div>';
+        }
+    },
     buildShots:function (shots, how) {
         var imgs = '';
         for (var i = 0; i < shotcount && shots && i < shots.length; i++) {
@@ -216,108 +265,113 @@ var getShots = {
             this.container.append(imgs);
         }
 
-        //insert add into a random position 2 per 11
-        var minRand = 3;
-        var maxRand = 12;
-        var ad = 'CK7IP';
-
-        //var min2Rand = 14;
-        //var max2Rand = 20;
-        //var ad2 = 'CVYI6';
-
         var injectAd = function (min, max, ad) {
             var rand = Math.random(); //return a decimal between 0 and 1
             var randSpan = rand * (max - min);
             var floor = Math.floor(randSpan);
             var pos = floor + min + ((page - 1) * 20);
 
+            console.log(singleShot);
+            var singleShotId = setInterval(function(){
+                if (singleShot == '') {
+                    return;
+                }
+                if(singleShot !== '') {
+                    $('#picsList div:nth-child(' + pos + ')').after(singleShot);
+                    clearInterval(singleShotId);
+                    //_gaq.push(['_trackEvent', 'ad', 'populated ad', adId]);
+                }
+            }, 100);
+
             //console.log('inject ad');
 
             // Insert Add Code Here
-            $.ajax({
-                type:'get',
-                url:'https://srv.buysellads.com/ads/' + ad + '.json',
-                success:Success,
-                error:Error,
-                dataType:'jsonp'
-            });
-            function Success(res) {
-
-                console.log(res);
-
-                /*if (!res.ads[0].shotId) {
-                 _gaq.push(['_trackEvent', 'ad', 'api no response', ad]);
-                 return;
-                 }*/
-
-                if(res.ads[0].statlink) {
-                    clickUrl = res.ads[0].statlink;
-                } else {
-                    clickUrl = res.ads[0].statlink_default;
-                } 
-                var adId = res.ads[0].shotId,
-                    //clickUrl = if(res.ads[0].statlink) {res.ads[0].statlink} else {res.ads[0].statlink_default},
-                    clickId = clickUrl.substring(clickUrl.lastIndexOf('/') + 1),
-                    shotUrl = drib.host + drib.shot + adId,
-                    yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from json where url="' + shotUrl + '"') + '&format=json&callback=cb',
-                    html = res.ads[0].html;
-
-
-                //console.log('AD ####'+ shotUrl);
-
-
-                $.ajax({
-                    url:yql,
-                    dataType:'jsonp',
-                    jsonp:true,
-                    jsonpCallback:'cb',
-                    timeout:1000,
-                    success:function (data) {
-                        console.log(data);
-                        var shotStr = data.query.results.json,
-                            singleShot = '';
-                        console.log(shotStr);
-                        console.log(res.ads[0].image);
-                        if(res.ads[0].image) {
-                            singleShot += '<div id="ad" class="animate fadeInUp item">' +
-                            '   <div id="fusion_ad" data-clickid="' + clickId + '">' +
-                            '       <span class="fusionentire">'+
-                            '           <a href="'+ res.ads[0].link+'" target="_blank">' +
-                            '               <img src=" '+ res.ads[0].image +' " class="fusionimg"  border="0" height="100" width="130">' +
-                            '           </a>' +
-                            '       <a href="'+ res.ads[0].link +'" class="fusiontext" title='+ res.ads[0].title +'" target="_top"> '+ res.ads[0].title +' </a>' +
-                            '       <p>'+ res.ads[0].description +'</p>'+
-                            '       </span>'+    
-                            '       <a class="poweredBy" href="http://buysellads.com/buy/detail/173658">Your Ad Here</a>' +
-                            '   </div>';
-                            '</div>';
-                        } else {
-                            singleShot += '' + res.ads[0].html + '<div id="ad" class="animate fadeInUp item"><div id="fusion_ad" data-clickid="' + clickId + '">' +
-                                '       <a class="poweredBy" href="http://fusionads.net">Powered by Fusion</a>' +
-                                '   </div>';
-                            '</div>';
-                        }
-                        $('#picsList div:nth-child(' + pos + ')').after(singleShot);
-                        _gaq.push(['_trackEvent', 'ad', 'populated ad', adId]);
-
-
-                    },
-                    error:function (x, t, m) {
-                        return;
-                    }
-
-                });
-            }
-
-            function Error(response) {
-                console.log('calling error', response);
-
-            }
+            //$.ajax({
+            //    type:'get',
+            //    url:'https://srv.buysellads.com/ads/' + ad + '.json',
+            //    success:Success,
+            //    error:Error,
+            //    dataType:'jsonp'
+            //});
+            //function Success(res) {
+//
+            //    //console.log(res);
+//
+            //    /*if (!res.ads[0].shotId) {
+            //     _gaq.push(['_trackEvent', 'ad', 'api no response', ad]);
+            //     return;
+            //     }*/
+//
+            //    if(res.ads[0].statlink) {
+            //        clickUrl = res.ads[0].statlink;
+            //    } else {
+            //        clickUrl = res.ads[0].statlink_default;
+            //    } 
+            //    var adId = res.ads[0].shotId,
+            //        //clickUrl = if(res.ads[0].statlink) {res.ads[0].statlink} else {res.ads[0].statlink_default},
+            //        clickId = clickUrl.substring(clickUrl.lastIndexOf('/') + 1),
+            //        shotUrl = drib.host + drib.shot + adId,
+            //        yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from json where url="' + shotUrl + '"') + '&format=json&callback=cb',
+            //        html = res.ads[0].html;
+//
+//
+            //    //console.log('AD ####'+ shotUrl);
+//
+//
+            //    $.ajax({
+            //        url:yql,
+            //        dataType:'jsonp',
+            //        jsonp:true,
+            //        jsonpCallback:'cb',
+            //        timeout:1000,
+            //        success:function (data) {
+            //            //console.log(data);
+            //            var shotStr = data.query.results.json,
+            //                singleShot = '';
+            //            //console.log(shotStr);
+            //            //console.log(res.ads[0].image);
+            //            if(res.ads[0].image) {
+            //                singleShot += '<div id="ad" class="animate fadeInUp item">' +
+            //                '   <div id="fusion_ad" data-clickid="' + clickId + '">' +
+            //                '       <span class="fusionentire">'+
+            //                '           <a href="'+ res.ads[0].link+'" target="_blank">' +
+            //                '               <img src=" '+ res.ads[0].image +' " class="fusionimg"  border="0" height="100" width="130">' +
+            //                '           </a>' +
+            //                '       <a href="'+ res.ads[0].link +'" class="fusiontext" title='+ res.ads[0].title +'" target="_top"> '+ res.ads[0].title +' </a>' +
+            //                '       <p>'+ res.ads[0].description +'</p>'+
+            //                '       </span>'+    
+            //                '       <a class="poweredBy" href="http://buysellads.com/buy/detail/173658">Your Ad Here</a>' +
+            //                '   </div>';
+            //                '</div>';
+            //            } else {
+            //                singleShot += '' + res.ads[0].html + '<div id="ad" class="animate fadeInUp item"><div id="fusion_ad" data-clickid="' + clickId + '">' +
+            //                    '       <a class="poweredBy" href="http://fusionads.net">Powered by Fusion</a>' +
+            //                    '   </div>';
+            //                '</div>';
+            //            }
+            //            $('#picsList div:nth-child(' + pos + ')').after(singleShot);
+            //            _gaq.push(['_trackEvent', 'ad', 'populated ad', adId]);
+//
+//
+            //        },
+            //        error:function (x, t, m) {
+            //            return;
+            //        }
+//
+            //    });
+            //}
+//
+            //function Error(response) {
+            ////    console.log('calling error', response);
+//
+//            }
         };
 
-        console.log('call ad injection' + page);
+        //console.log('call ad injection' + page);
         if (page === 1) {
-            injectAd(minRand, maxRand, ad);
+            var minRand = 3;
+            var maxRand = 12;
+            injectAd(minRand, maxRand);
         }
     }
 };
@@ -406,6 +460,16 @@ var ui = {
                 });
             }
         });
+        
+        themeInput.click(function(){
+            if(themeInput.is(':checked')) {
+                window.localStorage.setItem('theme', 'dark');
+                $('body').removeClass('white').addClass('dark');
+            } else {
+                window.localStorage.setItem('theme', 'white');
+                $('body').removeClass('dark').addClass('white');
+            }
+        });
 
         //On load 
         user.val(userName);
@@ -414,6 +478,7 @@ var ui = {
          } else {
          window.localStorage.setItem('current', userName);
          }*/
+
     }
 };
 
@@ -433,6 +498,7 @@ $(function () {
 
     // Set Active Nav Class
     drib.activeNav();
+    drib.activeTheme();
 
     addshots = setInterval(function () {
         //console.log('adding shots');
@@ -442,14 +508,14 @@ $(function () {
         diff = b + c;
         // only load more if there have been loaded some before:
 
-        if (diff >= a - 805 && $('#picsList').height() > 0) {
+        if (diff >= a - 1205 && $('#picsList').height() > 0) {
             from = localStorage.getItem('current');
             what = $("#what").text();
             page++;
             drib.add(page, from);
 
         }
-    }, 1500);
+    }, 500);
 
     $('.subInfo').hide();
 
@@ -476,26 +542,6 @@ $(function () {
             dataType:'jsonp'
         });
     })
-    /* click to close modal window
-     $(window).on('click', '.close', function () {
-     $(".modalOverlay").delay(250).fadeOut(250);
-     $(".modalContainer").addClass('out');
-     $('#picsList').removeClass('blur');
-     $(".close").fadeOut(250);
-
-     setTimeout(function () {
-     $(".modalContainer").fadeOut(250);
-     mainViewModel.modal().active(false);
-     }, 200);
-
-
-     });
-     $(".close").click(function () {
-     $(".overlay").delay(250).fadeOut(250);
-     $(".content").removeClass('in').addClass('out').css({
-     // "display" : "none"
-     });
-     });*/
 });
 
 var _gaq = _gaq || [];
